8a7721484a1c1242b0a1532f4505591039004e42
LudoBermejoES <LudoGithub@gmail.com>
2022-09-06 15:43:27 +0100

    Even more refactor

--- src/module/util/setup/hooks.ts
+++ src/module/util/setup/hooks.ts
@@ -1,4 +1,4 @@
-import { changeTokensSizeIfInTheSameGridPosition, storeOriginalPosition } from '../resize';
+import { changeTokensSizeIfInTheSameGridPosition, storeOriginalPosition } from '../../libs/resize';
 
 export function registerHooks(): void {
   Hooks.on('preUpdateToken', storeOriginalPosition);
